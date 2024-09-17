426a45077c93a59cb166ac28236dec811dad08a0
Briar <briar.j.mills@gmail.com>
2024-09-17 18:41:34 -0400

    Working version 1

--- src/hdx/scraper/icpac_cdi/__main__.py
+++ src/hdx/scraper/icpac_cdi/__main__.py
@@ -6,6 +6,7 @@ script then creates in HDX.
 """
 
 import logging
+from datetime import datetime
 from os.path import dirname, expanduser, join
 
 from hdx.api.configuration import Configuration
@@ -15,10 +16,11 @@ from hdx.utilities.path import (
     wheretostart_tempdir_batch,
 )
 from hdx.utilities.retriever import Retrieve
+from icpac_cdi import ICPAC_CDI
 
 logger = logging.getLogger(__name__)
 
-_USER_AGENT_LOOKUP = "hdx-scraper-icpac_cdi"
+_USER_AGENT_LOOKUP = "hdx-scraper-icpac-cdi"
 _SAVED_DATA_DIR = "saved_data"  # Keep in repo to avoid deletion in /tmp
 _UPDATED_BY_SCRIPT = "HDX Scraper: icpac_cdi"
 
@@ -48,21 +50,33 @@ def main(
                 use_saved=use_saved,
             )
             configuration = Configuration.read()
-            #
-            # Steps to generate dataset
-            #
-            dataset.update_from_yaml(
-                path=join(
-                    dirname(__file__), "config", "hdx_dataset_static.yaml"
+
+            icpac_cdi = ICPAC_CDI(
+                configuration,
+                retriever,
+                temp_dir,
+                datetime.today().year,
+            )
+
+            icpac_cdi.get_hdx_data()
+            dataset_names = icpac_cdi.get_data()
+
+            for dataset_name in dataset_names:
+                dataset = icpac_cdi.generate_dataset(dataset_name)
+
+                dataset.update_from_yaml(
+                    path=join(
+                        dirname(__file__), "config", "hdx_dataset_static.yaml"
+                    )
                 )
-            )
-            dataset.create_in_hdx(
-                remove_additional_resources=True,
-                match_resource_order=False,
-                hxl_update=False,
-                updated_by_script=_UPDATED_BY_SCRIPT,
-                batch=info["batch"],
-            )
+                dataset.create_in_hdx(
+                    remove_additional_resources=False,
+                    match_resource_order=False,
+                    hxl_update=False,
+                    updated_by_script=_UPDATED_BY_SCRIPT,
+                    batch=info["batch"],
+                    ignore_check=["resource:description"],
+                )
 
 
 if __name__ == "__main__":
--- src/hdx/scraper/icpac_cdi/icpac_cdi.py
+++ src/hdx/scraper/icpac_cdi/icpac_cdi.py
@@ -2,33 +2,109 @@
 """icpac_cdi scraper"""
 
 import logging
-from typing import Optional
+from datetime import datetime, timedelta
+from typing import List
+
+from bs4 import BeautifulSoup
+from dateutil.relativedelta import relativedelta
 
 from hdx.api.configuration import Configuration
 from hdx.data.dataset import Dataset
-from hdx.data.hdxobject import HDXError
+from hdx.data.resource import Resource
+from hdx.utilities.base_downloader import DownloadError
+from hdx.utilities.dictandlist import dict_of_lists_add
 from hdx.utilities.retriever import Retrieve
 
 logger = logging.getLogger(__name__)
 
 
-class icpac_cdi:
+class ICPAC_CDI:
     def __init__(
-        self, configuration: Configuration, retriever: Retrieve, temp_dir: str
+        self,
+        configuration: Configuration,
+        retriever: Retrieve,
+        temp_dir: str,
+        year: int,
     ):
         self._configuration = configuration
         self._retriever = retriever
         self._temp_dir = temp_dir
+        self._time_periods = configuration["time_periods"]
+        self._year = year
+        self.hdx_data = {}
+        self.data = {}
+        self.dates = {}
 
-    def generate_dataset(self) -> Optional[Dataset]:
-        # To be generated
-        dataset_name = None
-        dataset_title = None
-        dataset_time_period = None
-        dataset_tags = None
-        dataset_country_iso3 = None
+    def get_hdx_data(self) -> None:
+        for time_period in self._time_periods:
+            dataset_name = f"igad-region-{time_period}-combined-drought-indicator-cdi-{self._year}"
+            dataset = Dataset.read_from_hdx(dataset_name)
+            if not dataset:
+                continue
+            resources = dataset.get_resources()
+            self.hdx_data[dataset_name] = [
+                resource["name"] for resource in resources
+            ]
 
-        # Dataset info
+    def get_data(self) -> List[str]:
+        for time_period in self._time_periods:
+            logger.info(f"Downloading {time_period} data for {self._year}")
+            dataset_info = self._configuration[time_period]
+            dataset_name = f"igad-region-{time_period}-combined-drought-indicator-cdi-{self._year}"
+            base_url = f"{dataset_info['url']}{self._year}/"
+
+            try:
+                text = self._retriever.download_text(
+                    base_url, filename=f"{time_period}_{self._year}"
+                )
+            except DownloadError:
+                logger.error(f"Could not get data from {base_url}")
+                continue
+
+            soup = BeautifulSoup(text, "html.parser")
+            lines = soup.find_all("a")
+            for line in lines:
+                filename = line.get("href")
+                if filename[-3:] != "tif":
+                    continue
+                if filename in self.hdx_data.get(dataset_name, []):
+                    continue
+                file_url = f"{base_url}{filename}"
+                try:
+                    filepath = self._retriever.download_file(
+                        file_url, filename=filename
+                    )
+                except DownloadError:
+                    logger.error(f"Could not download file {filename}")
+                    continue
+
+                dict_of_lists_add(self.data, dataset_name, filepath)
+
+                file_date = filename.split(".")[0].split("-")[4:]
+                if time_period == "dekadal":
+                    date_start = datetime(
+                        self._year, int(file_date[0]), int(file_date[1])
+                    )
+                    date_end = date_start + timedelta(days=9)
+                if time_period == "monthly":
+                    date_start = datetime.strptime(
+                        f"{self._year} {file_date[0]} 1", "%Y %b %d"
+                    )
+                    date_end = date_start + relativedelta(day=31)
+                dict_of_lists_add(self.dates, dataset_name, date_start)
+                dict_of_lists_add(self.dates, dataset_name, date_end)
+
+        return [dataset_name for dataset_name in sorted(self.data)]
+
+    def generate_dataset(self, dataset_name: str) -> Dataset:
+        if "dekadal" in dataset_name:
+            time_period = "dekadal"
+        if "monthly" in dataset_name:
+            time_period = "monthly"
+
+        dataset_info = self._configuration[time_period]
+        dataset_title = f"{dataset_info['title']} {self._year}"
+
         dataset = Dataset(
             {
                 "name": dataset_name,
@@ -36,18 +112,26 @@ class icpac_cdi:
             }
         )
 
-        dataset.set_time_period(dataset_time_period)
-        dataset.add_tagsa(dataset_tags)
-        # Only if needed
+        date_start = min(self.dates[dataset_name])
+        date_end = max(self.dates[dataset_name])
+        dataset.set_time_period(startdate=date_start, enddate=date_end)
+
+        dataset["notes"] = dataset_info["notes"]
+        dataset.add_tags(self._configuration["tags"])
+        dataset.set_expected_update_frequency(dataset_info["update_frequency"])
         dataset.set_subnational(True)
-        try:
-            dataset.add_country_location(dataset_country_iso3)
-        except HDXError:
-            logger.error(
-                f"Couldn't find country {dataset_country_iso3}, skipping"
+        dataset.add_country_locations(self._configuration["countries"])
+
+        resources = []
+        resource_paths = self.data[dataset_name]
+        for resource_path in resource_paths:
+            resource = Resource(
+                {
+                    "name": resource_path.split("/")[-1],
+                    "format": "GeoTIFF",
+                }
             )
-            return
-
-        # Add resources here
-
+            resource.set_file_to_upload(resource_path)
+            resources.append(resource)
+        dataset.add_update_resources(resources)
         return dataset
